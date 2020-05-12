dac941888d84e8cad70d8bbe164cbac54b122714
NotMyLyfe <30644833+NotMyLyfe@users.noreply.github.com>
2020-05-12 16:38:08 -0400

    Changed scheme to https

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ Session(app)
 @app.route("/")
 def index():
     if not session.get("user"):
-        return redirect(url_for("login"))
+        return redirect(url_for("login", _scheme='https'))
     print(app.config.get("B2C_PROFILE_AUTHORITY"))
 
     return render_template('index.html', user=session["user"], version=msal.__version__)
@@ -27,7 +27,7 @@ def login():
 @app.route(app_config.REDIRECT_PATH)  # Its absolute URL must match your app's redirect_uri set in AAD
 def authorized():
     if request.args.get('state') != session.get("state"):
-        return redirect(url_for("index"))  # No-OP. Goes back to Index page
+        return redirect(url_for("index", _scheme='https'))  # No-OP. Goes back to Index page
     if "error" in request.args:  # Authentication/Authorization failure
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
@@ -35,25 +35,25 @@ def authorized():
         result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
             request.args['code'],
             scopes=app_config.SCOPE,  # Misspelled scope would cause an HTTP 400 error here
-            redirect_uri=url_for("authorized", _external=True))
+            redirect_uri=url_for("authorized", _external=True, _scheme='https'))
         if "error" in result:
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
         _save_cache(cache)
-    return redirect(url_for("index"))
+    return redirect(url_for("index", _scheme='https'))
 
 @app.route("/logout")
 def logout():
     session.clear()  # Wipe out user and its token cache from session
     return redirect(  # Also logout from your tenant's web session
         app_config.AUTHORITY + "/oauth2/v2.0/logout" +
-        "?post_logout_redirect_uri=" + url_for("index", _external=True))
+        "?post_logout_redirect_uri=" + url_for("index", _external=True, _scheme='https'))
 
 @app.route("/graphcall")
 def graphcall():
     token = _get_token_from_cache(app_config.SCOPE)
     if not token:
-        return redirect(url_for("login"))
+        return redirect(url_for("login", _scheme='https'))
     graph_data = requests.get(  # Use token to call downstream service
         app_config.ENDPOINT,
         headers={'Authorization': 'Bearer ' + token['access_token']},
