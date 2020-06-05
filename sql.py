660dd44702bab01664c2c18781a40649ae23c02b
NotMyLyfe <30644833+NotMyLyfe@users.noreply.github.com>
2020-06-04 20:55:08 -0400

    Created a new cursor so errors don't appear

--- sql.py
+++ sql.py
@@ -1,30 +1,33 @@
 import pyodbc
 import os
 
-cursor = pyodbc.connect(
+connection = pyodbc.connect(
     'DRIVER={ODBC Driver 17 for SQL Server};'
     'SERVER=officeconnected.database.windows.net;'
     'PORT=1433;'
     'DATABASE=OfficeConnected;'
     'UID=OfficeConnected;'
     'PWD='+os.getenv('SQL_PASSWORD')
-).cursor()
+)
 
-insert_query = '''INSERT INTO userData (Token, PhoneNumber, GetSMSTeamNotifications, Email, EmailOverSMS, VerifiedPhone, VerificationCode, ContinuedCommand) VALUES (?, ?, ?, ?, ?, ?, ?, ?);'''
-read_query = '''SELECT * FROM userData;'''
-readSpecific_query = '''SELECT * FROM userData WHERE Email LIKE ?;'''
-readSpecific_queryPhone = '''SELECT * FROM userData WHERE PhoneNumber = ?;'''
+cursor = connection.cursor()
+rcursor = connection.cursor()
+
+insert_query = '''SET NOCOUNT ON; INSERT INTO userData (Token, PhoneNumber, GetSMSTeamNotifications, Email, EmailOverSMS, VerifiedPhone, VerificationCode, ContinuedCommand) VALUES (?, ?, ?, ?, ?, ?, ?, ?);'''
+read_query = '''SET NOCOUNT ON; SELECT * FROM userData;'''
+readSpecific_query = '''SET NOCOUNT ON; SELECT * FROM userData WHERE Email LIKE ?;'''
+readSpecific_queryPhone = '''SET NOCOUNT ON; SELECT * FROM userData WHERE PhoneNumber = ?;'''
 update = {
-    'Token' : '''UPDATE userData SET Token = ? WHERE Email LIKE ?;''',
-    'PhoneNumber' : '''UPDATE userData SET PhoneNumber = ? WHERE Email LIKE ?;''',
-    'GetSMSTeamNotifications' : '''UPDATE userData SET GetSMSTeamNotifications = ? WHERE Email LIKE ?;''',
-    'EmailOverSMS' : '''UPDATE userData SET EmailOverSMS = ? WHERE Email LIKE ?;''',
-    'VerifiedPhone' : '''UPDATE userData SET VerifiedPhone = ? WHERE Email LIKE ?;''',
-    'VerificationCode' : '''UPDATE userData SET VerificationCode = ? WHERE Email LIKE ?;''',
-    'ContinuedCommand' : '''UPDATE userData SET ContinuedCommand = ? WHERE Email LIKE ?;'''
+    'Token' : '''SET NOCOUNT ON; UPDATE userData SET Token = ? WHERE Email LIKE ?;''',
+    'PhoneNumber' : '''SET NOCOUNT ON; UPDATE userData SET PhoneNumber = ? WHERE Email LIKE ?;''',
+    'GetSMSTeamNotifications' : '''SET NOCOUNT ON; UPDATE userData SET GetSMSTeamNotifications = ? WHERE Email LIKE ?;''',
+    'EmailOverSMS' : '''SET NOCOUNT ON; UPDATE userData SET EmailOverSMS = ? WHERE Email LIKE ?;''',
+    'VerifiedPhone' : '''SET NOCOUNT ON; UPDATE userData SET VerifiedPhone = ? WHERE Email LIKE ?;''',
+    'VerificationCode' : '''SET NOCOUNT ON; UPDATE userData SET VerificationCode = ? WHERE Email LIKE ?;''',
+    'ContinuedCommand' : '''SET NOCOUNT ON; UPDATE userData SET ContinuedCommand = ? WHERE Email LIKE ?;'''
 }
 
-delete_query = '''DELETE FROM userData WHERE Email LIKE ?;'''
+delete_query = '''SET NOCOUNT ON; DELETE FROM userData WHERE Email LIKE ?;'''
 
 def updateVal(Email, column, value):
     cursor.execute(update[column], (value, Email))
@@ -40,13 +43,13 @@ def insert(Token, Email):
 # 'all' : ''' UPDATE userData SET Token = ?, PhoneNumber = ?, GetSMSTeamNotifications = ?, EmailOverSMS = ? WHERE Email LIKE ?;'''
 
 def fetch(Email):
-    return cursor.execute(readSpecific_query, (Email))
+    return rcursor.execute(readSpecific_query, (Email))
 
 def fetchPhone(PhoneNumber):
-    return cursor.execute(readSpecific_queryPhone, (PhoneNumber))
+    return rcursor.execute(readSpecific_queryPhone, (PhoneNumber))
 
 def getAll():
-    return cursor.execute(read_query)
+    return rcursor.execute(read_query)
 
 def delete(Email):
     if fetch(Email).fetchone():
