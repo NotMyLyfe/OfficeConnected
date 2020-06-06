09415c8f9f58784a2f5aeea90e879d9b2eea1c34
NotMyLyfe <30644833+NotMyLyfe@users.noreply.github.com>
2020-06-05 21:28:32 -0400

    Adding comments to app_config

--- app_config.py
+++ app_config.py
@@ -1,24 +1,26 @@
+# app_config.py
+# Gordon Lin and Evan Lu
+# Configuration file for app.py with data about OAuth and Flask
 import os
 
+# Gets the CLIENT SECRET of Azure Active Directory (AAD) App (for OAuth)
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-if not CLIENT_SECRET:
-    raise ValueError("Need to define CLIENT_SECRET environment variable")
 
-AUTHORITY = "https://login.microsoftonline.com/organizations"  # For multi-tenant app
-# AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
+# Authority for AAD App (OAuth) login
+AUTHORITY = "https://login.microsoftonline.com/organizations"
 
+# Client Secret for AAD App (OAuth) login
 CLIENT_ID = "0d1fbbd3-699d-4f2a-8670-5c2ea4683074"
 
-REDIRECT_PATH = "/getAToken"  # It will be used to form an absolute URL
-    # And that absolute URL must match your app's redirect_uri set in AAD
+# OAuth redirect path after being logged in
+REDIRECT_PATH = "/getAToken"
 
-# You can find more Microsoft Graph API endpoints from Graph Explorer
-# https://developer.microsoft.com/en-us/graph/graph-explorer
+# Microsoft Graphs API endpoint
 ENDPOINT = 'https://graph.microsoft.com/beta'
 
-# You can find the proper permission names from this document
-# https://docs.microsoft.com/en-us/graph/permissions-reference
+# Scopes (permissions) to access Microsoft Graphs API
 SCOPE = ["User.ReadBasic.All", "User.Read.All", "Calendars.ReadWrite", "Files.ReadWrite.All", "Mail.ReadWrite", "Mail.Send", "People.Read.All", "Group.Read.All", "Group.ReadWrite.All"]
 
-SESSION_TYPE = "filesystem"  # So token cache will be stored in server-side session
+# Setting token cache to be stored in sever-side session
+SESSION_TYPE = "filesystem"
 
